2ab5853010b51c58ee6551467dffc125b2e5c82a
Love Khatri <lovekhatri9211@gmail.com>
2024-07-18 19:47:34 +0530

    Init

---
+++ README.md
@@ -0,0 +1,23 @@
+# learning-prisma
+
+1. Initializing Prisma
+`bunx prisma init`
+
+Initializing Prisma with a specific database
+`bunx prisma init --datasource-provider <DB_NAME>`
+> Example: `bunx prisma init --datasource-provider sqlite`
+
+2. Modifying the env variables
+`DATABASE_URL="YOUR_DATABASE_URL"`
+> Example: `DATABASE_URL="file:./dev.db"`
+
+3. Migrate the database
+`bunx prisma migrate dev --name <MIGRATION_NAME>`
+> Example: `bunx prisma migrate dev --name init`
+
+Migrate means to apply the changes to the database. It's like telling your database to create/alter the tables.
+
+4. Generate Prisma Client
+`bunx prisma generate`
+> Although the client is generated with the Migrate command as well, you can generate a new client if you don't want to apply the changes to the database.
+
---
+++ index.ts
@@ -0,0 +1,93 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+    // ? Create a user
+    // const user = await prisma.user.create({
+    //     data: {
+    //         name: "Amour",
+    //         email: "amour@mail.com",
+    //     }
+    // })
+
+    // console.log(user)
+
+    // ? Get all Users
+    // const users = await prisma.user.findMany()
+
+    // console.log(users)
+
+    // ? Create Article
+    // const article = await prisma.article.create({
+    //     data: {
+    //         title: "Hello World",
+    //         body: "This is a test article",
+    //         // author field doesn't directly contains an object, it links to a user, so we are linking it with the user id
+    //         author: {
+    //             connect: {
+    //                 id: 1
+    //             }
+    //         }
+    //     }
+    // })
+
+    // console.log(article)
+
+    // ? Get all Articles
+    // const articles = await prisma.article.findMany()
+
+    // console.log(articles)
+
+    // ? Create user and article and associate them
+    // const newUserWithNewArticle = await prisma.user.create({
+    //     data: {
+    //         name: "Not Amour",
+    //         email: "notamout@mail.com",
+    //         articles: {
+    //             create: {
+    //                 title: "Single Query Article",
+    //                 body: "This article was created alongside a user in a single query"
+    //             }
+    //         }
+    //     }
+    // })
+
+    // console.log(newUserWithNewArticle)
+
+    // ? Create an Article and also create a new user
+    // const newArticleWithNewUser = await prisma.article.create({
+    //     data: {
+    //         title: "Article 1",
+    //         body: "This is article 1",
+    //         author: {
+    //             create: {
+    //                 name: "Author 1",
+    //                 email: "Author@mail.com"
+    //             }
+    //         }
+    //     }
+    // })
+
+    // console.log(newArticleWithNewUser)
+
+    // ? Get all Users with their articles
+    // const usersWithArticles = await prisma.user.findMany({
+    //     // * Go to user.article and get all the articles and return them along with user data
+    //     include: {
+    //         articles: true
+    //     }
+    // })
+
+    // console.log(usersWithArticles)
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.log(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    })
